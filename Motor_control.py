55755d0b655c5d6de472945c10ece6845b7ff745
robanu <55018360+robanu@users.noreply.github.com>
2020-08-10 17:50:37 +0530

    Create Motor_control.py

---
+++ Motor_control.py
@@ -0,0 +1,58 @@
+from picamera.array import PiRGBArray    
+from picamera import PiCamera
+import RPi.GPIO as GPIO
+import time
+import cv2
+import numpy as np
+
+MOTOR1B=18  #Left Motor
+MOTOR1E=22
+
+MOTOR2B=21  #Right Motor
+MOTOR2E=19
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(MOTOR1B, GPIO.OUT)
+GPIO.setup(MOTOR1E, GPIO.OUT)
+
+GPIO.setup(MOTOR2B, GPIO.OUT)
+GPIO.setup(MOTOR2E, GPIO.OUT)
+
+def forward():
+      GPIO.output(MOTOR1B, GPIO.HIGH)
+      GPIO.output(MOTOR1E, GPIO.LOW)
+      GPIO.output(MOTOR2B, GPIO.HIGH)
+      GPIO.output(MOTOR2E, GPIO.LOW)
+      print("forward")
+
+     
+def reverse():
+      GPIO.output(MOTOR1B, GPIO.LOW)
+      GPIO.output(MOTOR1E, GPIO.HIGH)
+      GPIO.output(MOTOR2B, GPIO.LOW)
+      GPIO.output(MOTOR2E, GPIO.HIGH)
+      print("reverse")
+
+def rightturn():
+      GPIO.output(MOTOR1B,GPIO.LOW)
+      GPIO.output(MOTOR1E,GPIO.HIGH)
+      GPIO.output(MOTOR2B,GPIO.HIGH)
+      GPIO.output(MOTOR2E,GPIO.LOW)
+      print("right")
+     
+def leftturn():
+      GPIO.output(MOTOR1B,GPIO.HIGH)
+      GPIO.output(MOTOR1E,GPIO.LOW)
+      GPIO.output(MOTOR2B,GPIO.LOW)
+      GPIO.output(MOTOR2E,GPIO.HIGH)
+      print("left")
+
+def stop():
+      GPIO.output(MOTOR1E,GPIO.LOW)
+      GPIO.output(MOTOR1B,GPIO.LOW)
+      GPIO.output(MOTOR2E,GPIO.LOW)
+      GPIO.output(MOTOR2B,GPIO.LOW)
+      print("stop")
+while(True):
+    forward()
+    time.sleep(5)
